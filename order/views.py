0e2b278f6cb37c0c694b145a2b5eda25e6aaae70
Bonghyun Yang <shura@liberabit.co.kr>
2021-03-15 17:23:33 +0900

    3월 15일 작업내용

--- order/views.py
+++ order/views.py
@@ -1,1 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from user.decorators import login_required
+from django.db import transaction
+from .forms import RegisterForm
+from .models import Order
+from user.models import User
+from product.models import Product
+
+
+@method_decorator(login_required, name='dispatch')
+class OrderCreate(FormView):
+    form_class = RegisterForm
+    success_url = '/product/'
+
+    def form_valid(self, form):
+        with transaction.atomic():
+            prod = Product.objects.get(pk=form.data.get('product'))
+            order = Order(
+                quantity=form.data.get('quantity'),
+                product=prod,
+                order_user=User.objects.get(email=self.request.session.get('order_user'))
+            )
+        order.save()
+        prod.stock -= int(form.data.get('quantity'))
+        prod.save()
+
+        return super().form_valid(form)
+
+
+def form_invalid(self, form):
+    return redirect('/product/' + str(form.data.get('product')))
+
+
+def get_form_kwargs(self, **kwargs):
+    kw = super().get_form_kwargs(**kwargs)
+    kw.update({
+        'request': self.request
+    })
+    return kw
+
+
+@method_decorator(login_required, name='dispatch')
+# 클래스 안에 디스패치 함수가 있으며, 이를 감싸주는 데코레이터를 구현
+class OrderList(ListView):
+    model = Order
+    template_name = 'order.html'
+    context_object_name = 'order_list'
+
+    def get_queryset(self, **kwargs):
+        queryset = Order.objects.filter(order_user__email=self.request.session.get('user'))
+        return queryset
--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from .forms import RegisterForm, LoginForm
+from django.contrib.auth.hashers import make_password
+from .models import User
+
 
 
 def index(request):
@@ -12,6 +15,16 @@ class RegisterView(FormView):
     form_class = RegisterForm
     success_url = '/'
 
+    def form_valid(self, form):
+        user = User(
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password')),
+            level='user'
+        )
+        user.save()
+
+        return super().form_valid(form)
+
 
 class LoginView(FormView):
     template_name = 'login.html'
@@ -20,5 +33,13 @@ class LoginView(FormView):
 
     # 로그인이 정상적으로 되었을 때 동작
     def form_valid(self, form):
-        self.request.session['user'] = form.email
+        self.request.session['user'] = form.data.get('email')
+
         return super().form_valid(form)
+
+
+def logout(request):
+    if 'user' in request.session:
+        del (request.session['user'])
+
+    return redirect('/')
